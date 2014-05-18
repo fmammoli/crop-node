2ba2565f8879ac016b72e5f2ccc89204532ca6c4
fmammoli <fmammoli@gmail.com>
2014-05-18 12:31:45 -0300

    removing debugger

--- app/scripts/controllers/signup.js
+++ app/scripts/controllers/signup.js
@@ -47,7 +47,6 @@ angular.module('cropNodeApp')
 
           // Update validity of form fields that match the mongoose errors
           angular.forEach(err.errors, function(error, field) {
-            debugger;
             form[field].$setValidity('mongoose', false);
             $scope.errors[field] = error.message;
           });
